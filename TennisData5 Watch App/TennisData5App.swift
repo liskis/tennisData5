a8df81c4289c5b3257a2170be6919e4294c3e95e
liskis <watanabe@liskis.org>
2023-12-31 22:39:48 +0900

    Initial Commit

---
+++ TennisData5 Watch App/TennisData5App.swift
@@ -0,0 +1,17 @@
+//
+//  TennisData5App.swift
+//  TennisData5 Watch App
+//
+//  Created by 渡辺健輔 on 2023/12/31.
+//
+
+import SwiftUI
+
+@main
+struct TennisData5_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TennisData5/TennisData5App.swift
@@ -0,0 +1,17 @@
+//
+//  TennisData5App.swift
+//  TennisData5
+//
+//  Created by 渡辺健輔 on 2023/12/31.
+//
+
+import SwiftUI
+
+@main
+struct TennisData5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
